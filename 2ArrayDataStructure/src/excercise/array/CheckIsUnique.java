955b00e1275f8e957510d7b5375cf31b9e69d24c
BunHor-dev <bunhordigital77@gmail.com>
2021-09-16 05:29:03 +0700

    learn recursive

--- 2ArrayDataStructure/src/excercise/array/CheckIsUnique.java
+++
@@ -1,9 +0,0 @@
-package excercise.array;
-
-public class CheckIsUnique {
-    public static void main(String [] args){
-        Functions functions = new Functions();
-        int[] intArray = {6,2,9,3,4,5,6};
-        System.out.println(functions.isUnique(intArray));
-    }
-}
